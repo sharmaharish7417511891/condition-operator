9999edee73a5be98eda13841510ca824ac6632f0
sharmaharish7417511891 <112941880+sharmaharish7417511891@users.noreply.github.com>
2022-09-15 14:02:12 +0530

    Add files via upload

---
+++ ADMISSON BTECH.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int phy,che,mth;
+	printf("enter any no:");
+	scanf("%d",&phy);
+	printf("enter any no :");
+	scanf("%d",&che);
+	printf("enter any no :");
+	scanf("%d",&mth);
+	if(phy>=65)
+	{
+		if(che>=75)
+		{
+			if(mth>=85)
+			{
+				printf("%d BTECH ADMISSION IS ELIGABLE");
+			}
+		}
+	}
+	}
