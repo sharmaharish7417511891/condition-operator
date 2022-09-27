e3eafd8a6aa18487722cc1449481007f5c3e73d0
Harish kumar sharma <112941880+sharmaharish7417511891@users.noreply.github.com>
2022-09-27 21:41:57 +0530

    Add files via upload

---
+++ 4 NO COMPAIR.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	a=15;
+	b=10;
+	c=58;
+	d=10;
+	if (a>b)
+	{
+		if(a>c)
+		{
+			if(a>d)
+			{
+				printf("%d a is greater",a);
+			}
+		}
+	}
+	
+	
+
+
+	
+	
+	
+if(b>a)
+{
+	if(b>c)
+	{
+		if(b>d){
+			
+		}
+	}
+	}	
+	if(c>a)
+	{
+		if(c>b)
+		{
+			if(c>d)
+			{
+				printf("%d c is greater",c);
+			}
+		}
+	}
+	if(d>a)
+	{
+		if(d>b)
+		{
+			if(d>c)
+			{
+				printf("%d d is greater",d);
+			}
+		}
+	}
+}
